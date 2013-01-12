0988e0ee1a25c51a4f2baeddf1b30ca24174cfcb
Michael Leaney <leahcimic@gmail.com>
2013-01-12 22:38:45 +1100

    Add queue memory Add queue memory unit test Add index.js for initializing fetchers

---
+++ index.js
@@ -0,0 +1,21 @@
+var Config = require('./config.js');
+global.config = Config.get();
+
+var fs = require('fs');
+
+fs.readdir(
+	'./fetchers',
+	function(err, files) {
+		_.each(
+			files,
+			function(file, i) {
+				var name, fetcher;
+				if ((name = file.match(/(.*)\.js/))) {
+					fetcher = require('./fetchers/' + file);
+					fetcher = new fetcher({name: name[1]});
+					fetcher.initialize();
+				}
+			}
+		);
+	}
+);
