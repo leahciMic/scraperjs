3714721b8e6a6d25b4889a781f922cf7bc0ed30f
Michael Leaney <leahcimic@gmail.com>
2013-01-12 22:51:32 +1100

    Import fetcher and parser from old repo (still broken) Fix whitespace on a few files

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ global.config = Config.get();
 var fs = require('fs');
 
 fs.readdir(
-	'./fetchers',
-	function(err, files) {
-		_.each(
-			files,
-			function(file, i) {
-				var name, fetcher;
-				if ((name = file.match(/(.*)\.js/))) {
-					fetcher = require('./fetchers/' + file);
-					fetcher = new fetcher({name: name[1]});
-					fetcher.initialize();
-				}
-			}
-		);
-	}
+  './fetchers',
+  function(err, files) {
+    _.each(
+      files,
+      function(file, i) {
+        var name, fetcher;
+        if ((name = file.match(/(.*)\.js/))) {
+          fetcher = require('./fetchers/' + file);
+          fetcher = new fetcher({name: name[1]});
+          fetcher.initialize();
+        }
+      }
+    );
+  }
 );
--- specs/queue-memory.spec.js
+++ specs/queue-memory.spec.js
@@ -1,41 +1,41 @@
 describe('Memory Queue', function() {
-	var QM = require('../lib/queue-memory.js');
-	var qm = new QM();
+  var QM = require('../lib/queue-memory.js');
+  var qm = new QM();
 
-	it('Initial length should be zero', function() {
-		expect(qm.length()).toEqual(0);
-	});
+  it('Initial length should be zero', function() {
+    expect(qm.length()).toEqual(0);
+  });
 
   it('Add an item', function() {
-		qm.add('http://example.com', 'test');
+    qm.add('http://example.com', 'test');
     expect(qm.length()).toEqual(1);
-		qm.add('http://example.com/2', 'test2');
+    qm.add('http://example.com/2', 'test2');
     expect(qm.length()).toEqual(2);
   });
 
-	it('Item exists', function() {
-		expect(
-			qm.exists('http://example.com')
-		).toEqual(true);
-	});
+  it('Item exists', function() {
+    expect(
+      qm.exists('http://example.com')
+    ).toEqual(true);
+  });
 
-	it('Get all items', function() {
-		var urls = qm.getAll();
-		expect(urls[0].url).toEqual('http://example.com');
-		expect(urls[0].callback).toEqual('test');
-		expect(urls[1].url).toEqual('http://example.com/2');
-		expect(urls[1].callback).toEqual('test2');
-	});
+  it('Get all items', function() {
+    var urls = qm.getAll();
+    expect(urls[0].url).toEqual('http://example.com');
+    expect(urls[0].callback).toEqual('test');
+    expect(urls[1].url).toEqual('http://example.com/2');
+    expect(urls[1].callback).toEqual('test2');
+  });
 
-	it('Get an item', function() {
-		var qi = qm.get();
-		expect(qm.length()).toEqual(1);
-		expect(qi.url).toEqual('http://example.com');
-		expect(qi.callback).toEqual('test');
-	});
+  it('Get an item', function() {
+    var qi = qm.get();
+    expect(qm.length()).toEqual(1);
+    expect(qi.url).toEqual('http://example.com');
+    expect(qi.callback).toEqual('test');
+  });
 
-	it('Clear items', function() {
-		qm.clear();
-		expect(qm.length()).toEqual(0);
-	});
+  it('Clear items', function() {
+    qm.clear();
+    expect(qm.length()).toEqual(0);
+  });
 });
