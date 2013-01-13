a6f5836dd927b6fbdf96b2d1689599100b5cb030
Michael Leaney <leahcimic@gmail.com>
2013-01-13 23:37:17 +1100

    Add more tests

--- specs/fetcher.spec.js
+++ specs/fetcher.spec.js
@@ -1,12 +1,70 @@
 describe('Fetcher', function() {
   var Fetcher = require('../lib/fetcher.js');
   var fetcher = new Fetcher({name: 'test'});
-  it('Fetch http://google.com', function(done) {
+
+  var queueItem = {
+    url: 'http://www.google.com',
+    callback: 'test'
+  };
+
+  it('jQuery returns jQuery object', function(done) {
+    fetcher.getjQuery(
+      '<html><head></head><body><div>Test</div></body></html>',
+      function($) {
+        expect($('div').text()).toEqual('Test');
+        done();
+      }
+    );
+  });
+
+  it('Get base url', function(done) {
+    fetcher.getjQuery(
+      '<html><head><base href="http://example.com" /></head><body><div>Test</div></body></html>',
+      function($) {
+        expect(
+          fetcher.getBaseUrl($, queueItem)
+        ).toEqual('http://example.com/');
+        $('base').attr('href', 'http://example.com/');
+        expect(
+          fetcher.getBaseUrl($, queueItem)
+        ).toEqual('http://example.com/');
+        $('base').attr('href', 'http://example.com/test/');
+        expect(
+          fetcher.getBaseUrl($, queueItem)
+        ).toEqual('http://example.com/test/');
+        $('base').remove();
+        expect(
+          fetcher.getBaseUrl($, queueItem)
+        ).toEqual('http://www.google.com/');
+        done();
+      }
+    );
+  });
+
+  it('Normalize url', function(done) {
+    fetcher.getjQuery(
+      '<html><head><base href="http://example.com" /></head><body><div>Test</div></body></html>',
+      function($) {
+        expect(
+          fetcher.normalizeUrl('test', $, queueItem)
+        ).toEqual('http://example.com/test');
+        $('base').attr('href', 'http://example.com/example');
+        expect(
+          fetcher.normalizeUrl('test', $, queueItem)
+        ).toEqual('http://example.com/example/test');        
+        $('base').remove();
+        expect(
+          fetcher.normalizeUrl('test', $, queueItem)
+        ).toEqual('http://www.google.com/test');
+        done();
+      }  
+    );
+  });
+
+  
+
+  it('Fetch http://www.google.com', function(done) {
     runs(function() {
-      var queueItem = {
-        url: 'http://www.google.com',
-        callback: 'test'
-      };
       fetcher.fetch(
         queueItem,
         function(html, response) {
