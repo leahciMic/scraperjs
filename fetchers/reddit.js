84fbad090b7174e8faf684b2e648286605f6a35c
Michael Leaney <leahcimic@gmail.com>
2013-01-29 21:17:18 +1100

    Add reddit fetcher

---
+++ fetchers/reddit.js
@@ -0,0 +1,27 @@
+var Fetcher = require('../lib/fetcher.js');
+
+var reddit = Fetcher.extend({
+	home: {
+		'links': {
+			'div.sr-list ul.flat-list li a': 'home'
+		},
+		'blocks': [{
+			'name': 'posts',
+			'root': 'div.thing:has(a.title)',
+			'data': {
+				'title': 'a.title',
+				'link': ['a.title', function($) { return $(this).attr('href'); }],
+				'upvotes': ['', function($) { return $(this).attr('data-ups'); }],
+				'downvotes': ['', function($) { return $(this).attr('data-downs'); }]
+			}
+		}]
+	},
+	initialize: function() {
+		this.queue.add(
+			'http://www.reddit.com/',
+			'home'
+		);
+	}
+});
+
+module.exports = reddit;
