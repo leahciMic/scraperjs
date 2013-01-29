a88ac337964e86c1415e66aaa6b8976648e2f0d3
Michael Leaney <leahcimic@gmail.com>
2013-01-29 21:16:54 +1100

    Add file datastore (including tests)

---
+++ specs/datastore-file.spec.js
@@ -0,0 +1,58 @@
+
+describe('Fetcher', function() {
+  var Datastore = require('../lib/datastore-file.js'),
+      fs = require('fs');
+
+  var datastore = new Datastore({
+    saveTo: 'datastore-file-test/'
+  });
+
+  it(
+    'Datastore to create saveTo path',
+    function() {
+      expect(fs.existsSync(datastore.config.saveTo)).toEqual(true);
+    }
+  );
+
+  it(
+    'mkdirr to create directory 3 levels deep',
+    function() {
+      expect(datastore.mkdirr('mkdirr/test/directory')).toEqual(true);
+      expect(fs.existsSync('mkdirr/test/directory')).toEqual(true);
+      fs.rmdirSync('mkdirr/test/directory');
+      fs.rmdirSync('mkdirr/test');
+      fs.rmdirSync('mkdirr');
+    }
+  );
+
+  it(
+    'mkdirr to create directory 1 level deep',
+    function() {
+      expect(datastore.mkdirr('mkdirr-test')).toEqual(true);
+      expect(fs.existsSync('mkdirr-test')).toEqual(true);
+      fs.rmdirSync('mkdirr-test');
+    }
+  ); 
+
+  it('Add data to datastore', function(done) {
+    datastore.add(
+      'test-fetcher',
+      {url: 'http://example.com'},
+      {
+        test: 'test'
+      },
+      function(error) {
+        var datastr = fs.readFileSync(datastore.getFilename('test-fetcher'), 'utf8');
+        expect(datastr).toEqual(
+          '{"queueItem":{"url":"http://example.com"},"data":{"test":"test"}}' + "\r\n"
+        );
+        fs.unlinkSync(datastore.getFilename('test-fetcher'));
+        done();
+      }
+    );
+  });
+
+  it('remove datastore-file-test directory', function() {
+    fs.rmdir('datastore-file-test');
+  });
+});
